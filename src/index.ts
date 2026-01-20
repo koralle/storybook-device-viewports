5dd9b0f3ba6c6dc3a5bf4bc037012a8ddbf50eee
koralle <koralle+github@koralle-mgmg.com>
2026-01-20 19:30:21 +0900

    feat: fix typo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { ViewportMap } from "storybook/viewport";
 import { APPLE_DEVICE_VIEWPORTS } from "./apple";
 import { GOOGLE_DEVICE_VIEWPORTS } from "./google";
 
-export const AWESOME_DEVICE_VIEPORTS = {
+export const AWESOME_DEVICE_VIEWPORTS = {
   ...APPLE_DEVICE_VIEWPORTS,
   ...GOOGLE_DEVICE_VIEWPORTS,
 } as const satisfies ViewportMap;
